2cd4f4e64fa9654fce5f59c40d1019d380f862f7
sumanga wimaladasa <sumanga0000@gmail.com>
2024-01-29 10:53:02 +0530

    edited

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-const express = require('express')
-const app = express()
-const cors=require('cors');
+
+var express = require('express')
+var cors = require('cors')
+var app = express()
+app.use(express.json())
 app.use(cors({origin:'*'}));
-app.get('/',(req,res)=>{
-    console.log(req.method);
-    res.end("hello");
+
+// for arduino codes
+// app.get('/',(req,res)=>{
+//     console.log(req.method);
+//     res.end("hello");
+// })
+// app.listen(8090,()=>console.log("Server is running!!!"))
+
+
+
+
+
+// This is my backend API
+
+app.post('/', function (req, res) {
+ 
+    req.body; // { answer: 42 }
+    res.json(req.body);
+    console.log(req.body)
 })
-app.listen(8090,()=>console.log("Server is running!!!"))
+app.get('/',(req,res)=>{
+    console.log("hello")
+    res.end('get responde')
+})
+
+app.listen(3001, function () {
+  console.log('CORS-enabled web server listening on port 3001')
+})
