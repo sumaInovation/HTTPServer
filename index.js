052da3680d4aee5499f829f680afe5dc8937abbd
sumanga wimaladasa <sumanga0000@gmail.com>
2024-01-29 17:54:18 +0530

    edited

--- index.js
+++ index.js
@@ -1,38 +1,74 @@
+var express = require("express");
+var cors = require("cors");
+var app = express();
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+const mongoose = require("mongoose");
+const dbUrl =
+  "mongodb+srv://suma:123@cluster0.wxldekr.mongodb.net/?retryWrites=true&w=majority";
+const Schema = mongoose.Schema;
+// Name, Number,Course, email, password
+let newuserSchema = new Schema({
+  Name: {
+    type: String,
+  },
+  Number: {
+    type: String,
+  },
+  Course: {
+    type: String,
+  },
+  Email: {
+    type: String,
+  },
+  Password: {
+    type: String,
+  },
+});
+const userDeatils = mongoose.model("newuser", newuserSchema);
 
-var express = require('express')
-var cors = require('cors')
-var app = express()
-app.use(express.json())
-app.use(cors({origin:'*'}));
-
-// for arduino codes
-// app.get('/',(req,res)=>{
-//     console.log(req.method);
-//     res.end("hello");
-// })
-// app.listen(8090,()=>console.log("Server is running!!!"))
-
-
-
-
+mongoose
+  .connect(dbUrl)
+  .then(() => console.log("connected"))
+  .catch((error) => console.log(error));
 
 // This is my backend API
 
-app.post('/', function (req, res) {
- 
-    req.body; // { answer: 42 }
-    res.json(req.body);
-    console.log(req.body)
-})
-app.get('/',(req,res)=>{
-    console.log("hello")
-    res.end('get responde')
-})
-app.get('/test',(req,res)=>{
-  console.log("hello")
-  res.end('get test responde')
-})
+app.get("/", (req, res) => {
+  console.log("hello");
+  res.end("get responde");
+});
+app.post("/registration", (req, res) => {
+  mongoose
+    .connect(dbUrl)
+    .then(() => console.log("connected"))
+    .catch((error) => console.log(error));
+
+  userDeatils
+    .find({ Email: req.body.Email })
+    .then((result) => {
+      if (result.length === 0) {
+        console.log("Successfuly");
+        const userDeatils1 = new userDeatils({
+          Name: req.body.Name,
+          Course: req.body.Course,
+          Number: req.body.Number,
+          Email: req.body.Email,
+          Password: req.body.Password,
+        });
+        userDeatils1.save();
+        res.send("Successfuly");
+      } else {
+        console.log("Already exsiting");
+        res.send("Already exsiting");
+      }
+    })
+    .catch((error) => console.log(error));
+  
+});
+
+
 
 app.listen(3001, function () {
-  console.log('CORS-enabled web server listening on port 3001')
-})
+  console.log("CORS-enabled web server listening on port 3001");
+});
